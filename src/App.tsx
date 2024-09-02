a200bc04aedd299e13e2267c947711ba06271d5e
c3ll256 <candycell256@gmail.com>
2024-09-02 18:43:42 +0800

    feat: 基本完成控制界面样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PointcloudCollision from "./pages/pointcloud-collision";
 import Index from "./pages";
-
+import { useEffect } from "react";
 
 const App = () => {
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "black";
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/" element={<PointcloudCollision />} />
+          <Route path="/pcc" element={<PointcloudCollision />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,41 @@
+import { FolderOpenIcon, SaveIcon } from "lucide-react";
+import Control from "@/components/control";
+
 const Index = () => {
-  return <div>Index</div>;
+  return (
+    <div className="h-screen w-screen text-white flex flex-col">
+      {/* header */}
+      <header className="relative h-16 w-full px-10 flex justify-center items-center border-b-[2px] border-[#5753C6]">
+        <div className="absolute h-full left-10 top-0 flex justify-center items-center gap-5">
+          <div className="text-xl">工程1</div>
+          <div className="text-sm px-3 py-1 rounded-full bg-[#0D3C48]">FOMULAR 验证</div>
+        </div>
+
+        <div className="text-2xl">
+          XONAR
+        </div>
+
+        <div className="absolute h-full right-10 top-0 flex items-center justify-center gap-5">
+          <div className="bg-[#14151C] rounded-full p-3">
+            <SaveIcon />
+          </div>
+          <div className="bg-[#14151C] rounded-full p-3">
+            <FolderOpenIcon />
+          </div>
+        </div>
+      </header>
+
+      {/* adjust */}
+      <div className="w-full h-48 flex items-center justify-center">
+        adjust
+      </div>
+
+      {/* control */}
+      <div className="relative w-full">
+        <Control />
+      </div>
+    </div>
+  );
 };
 
 export default Index;
